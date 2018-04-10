2e116c652e29d195c97bb9c52ab8b0c9b9a41653
Monday Ayewa (Pan African Center for Empower) <v-moayew@microsoft.com>
2018-04-09 18:59:01 -0700

    Committing initial work for interfacing the project

---
+++ Blackjack/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{    /// <summary>
+    /// 
+    /// </summary>
+    class GameManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private List<IPlayer> player;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private IDeck deck;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public GameManager()
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
