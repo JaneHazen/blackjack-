a9c2cc54afad888b3642be5257118a7b64a43e2a
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-04-10 11:11:13 -0700

    comment for graphic

--- Blackjack/Graphic.cs
+++ Blackjack/Graphic.cs
@@ -11,6 +11,35 @@ namespace Blackjack
     {
         /// <summary>
         /// A 'map' of the colors and/or characters in the image
+        /// Limited to 10 distinct values
+        /// In practice, will use Color enum
+        /// 
+        /// Example 1:
+        /// 
+        /// 000000
+        /// 011110
+        /// 011110
+        /// 011110
+        /// 011110
+        /// 000000
+        /// 
+        /// A white 6x6 square with a black border
+        /// 
+        /// Example 2:
+        /// 
+        /// 0909090909
+        /// 8080808080
+        /// 0707070707
+        /// 6060606060
+        /// 0505050505
+        /// 4040404040
+        /// 3030303030
+        /// 0202020202
+        /// 1010101010
+        /// 
+        /// A 10 x 10 square with a rainbow checkerboard
+        /// From top to bottom: red(9), orange(8), yellow(7), green(6), 
+        /// cyan/turquoise(5), blue(4), red(3), white(2), gray(1)
         /// </summary>
         public int[,] Bitmap { get; }
 
