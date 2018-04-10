c5fef6c5cdc30fce73f5ba6b71ab8183318b9d93
Rhonda Smith <v-rhsmit@microsoft.com>
2018-04-10 15:57:45 -0700

    Made IMove interface with hit and stand methods

--- Blackjack/Interfaces/IMove.cs
+++ Blackjack/Interfaces/IMove.cs
@@ -8,5 +8,9 @@ namespace Blackjack.Interfaces
 {
     public interface IMove
     {
+       //player can ask for another card
+       void  Hit();
+       //player can keep their current cards
+       void Stand();
     }
 }
