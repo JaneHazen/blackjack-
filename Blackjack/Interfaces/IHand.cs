bd10eabbcabed25795b65b52e2e112fe94975806
v-janwil <iamjane@gmail.com>
2018-04-09 17:58:39 -0700

    Add hand interface

---
+++ Blackjack/Interfaces/IHand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{
+    //contains the player(or dealer)s' cards
+    interface IHand
+    {
+        // initiates a list of cards (preferably two?) that will compromise the player's hand
+        List<ICard> Hand { get; set; }
+        // loops through the hand to display each card
+        void DisplayHand();
+    }
+}
