fe1ecfc6f994deea42ce8565203913d889dacd98
v-hara <sow.haby@gmail.com>
2018-04-10 09:52:03 -0700

    Adding interface method stubs

--- Blackjack/Interfaces/IDeck.cs
+++ Blackjack/Interfaces/IDeck.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Blackjack.Interfaces
 {
+    /// <summary>
+    ///  A 52-card deck
+    /// </summary>
     interface IDeck
     {
+       /// <summary>
+       ///  All the cards in the deck
+       /// </summary>
+       List<ICard> Cards { get; }
+       
+
+        /// <summary>
+       ///  Deal out the cards
+       /// </summary>
+       //ICard Deal();
+
+        /// <summary>
+       ///  Check if card deck has cards
+       /// </summary>
+
+        /// <summary>
+       /// Shuffle the deck
+       /// </summary>
+
+        /// <summary>
+       ///  Hand 
+       /// </summary>
     }
 }
