ecf507bcd730a5dfbafc1a8a3c3ee2bbb98cc575
Liseth Cardozo Sejas (InConsulting Inc.) <v-licard@microsoft.com>
2018-04-10 17:47:59 -0700

    Add Unit test for Card Class

---
+++ Blackjack.Tests/CardTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void TestCardConstructor()
+        {
+            //Arrange 
+            var expectedSuit = Interfaces.CardSuit.Club;
+            var expectedName = "King";
+            var expectedValue = 10;
+            var expectedIsHidden = true;
+
+            var name = "King";
+            var value = 10;
+
+            //Act 
+            Card myCard = new Card(Interfaces.CardSuit.Club, name, value);
+
+            //Arrange 
+            Assert.AreEqual(expectedSuit, myCard.Suit);
+            Assert.AreEqual(expectedName, myCard.Name);
+            Assert.AreEqual(expectedValue, myCard.Value);
+            Assert.AreEqual(expectedIsHidden, myCard.IsHidden);
+            
+        }
+    }
+}
